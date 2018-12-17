fb8a1d5252b2614e2ec3fa88c2f949807b445308
cppbitman <daihongjun@kedacom.com>
2018-12-17 16:22:10 +0800

    windows build done

--- conanfile.py
+++ conanfile.py
@@ -1,27 +1,40 @@
 from conans import ConanFile, CMake, tools
+from conanos.build import config_scheme
 import os
-import stat
-import fnmatch
+
 
 class TaglibConan(ConanFile):
     name = "taglib"
     version = "1.11.1"
     description = "TagLib is a library for reading and editing the meta-data of several popular audio formats"
     url = "https://github.com/conanos/taglib"
-    license = "LGPLv2_1"
-    settings = "os", "compiler", "build_type", "arch"
-    options = {"shared": [True, False]}
-    default_options = "shared=True"
+    homepage = "https://taglib.org/"
+    license = "LGPL-2.1"
+    exports = ["COPYING.LGPL"]
     generators = "cmake"
-    requires = "zlib/1.2.11@conanos/dev"
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = "shared=True", "fPIC=True"
 
-    source_subfolder = "source_subfolder"
+    requires = "zlib/1.2.11@conanos/stable"
+
+    _source_subfolder = "source_subfolder"
+    _build_subfolder = "build_subfolder"
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            self.options.remove("fPIC")
+
+    def configure(self):
+        del self.settings.compiler.libcxx
+
+        config_scheme(self)
 
     def source(self):
-        url_ = 'http://{0}.org/releases/{0}-{1}.tar.gz'.format(self.name, self.version)
-        tools.get(url_)
+        url_ = 'https://taglib.org/releases/{name}-{version}.tar.gz'
+        tools.get(url_.format(name=self.name, version=self.version))
         extracted_dir = self.name + "-" + self.version
-        os.rename(extracted_dir, self.source_subfolder)
+        os.rename(extracted_dir, self._source_subfolder)
 
     def build(self):
         #self.run('cmake -DCMAKE_INSTALL_PREFIX=%s/build -DCMAKE_LIBRARY_OUTPUT_PATH=%s/build/lib'
@@ -34,34 +47,25 @@ class TaglibConan(ConanFile):
         #self.run('make install')
         # 
         
-        print "------", self.package_folder, self.install_folder
-        
-        with tools.chdir(self.source_subfolder):
+        with tools.chdir(self._source_subfolder):
             cmake = CMake(self)
             cmake.definitions["CMAKE_INSTALL_PREFIX"] = '%s/builddir/install'%(os.getcwd())
             cmake.definitions["WITH_MP4"] = 'ON'
             cmake.definitions["WITH_ASF"] = 'ON'
             cmake.definitions["CMAKE_DISABLE_FIND_PACKAGE_Boost"] = 'TRUE'
             cmake.definitions["ZLIB_ROOT"] = self.deps_cpp_info["zlib"].rootpath
-            #cmake.definitions["CMAKE_C_COMPILER"] = 'gcc'
-            #cmake.definitions["CMAKE_CXX_COMPILER"] = 'g++'
-            #cmake.definitions["CMAKE_C_FLAGS"] = '-Wall -g -O2 -m64  -Wall -g -O2 -m64  -Wall -g -O2 -m64'
-            #cmake.definitions["CMAKE_CXX_FLAGS"] = '-Wall -g -O2 -m64  -Wall -g -O2 -m64  -Wall -g -O2 -m64'
             if self.options.shared:
                 cmake.definitions["BUILD_SHARED_LIBS"] = 1
             else:
                 cmake.definitions["BUILD_STATIC_LIBS"] = 1
-            cmake.configure(source_folder=self.source_subfolder,
-                            build_folder='%s/builddir'%(self.source_subfolder))
+            cmake.configure(source_folder=self._source_subfolder,
+                            build_folder='%s/builddir'%(self._source_subfolder))
             cmake.build()
             cmake.install()
 
     def package(self):
-        if tools.os_info.is_linux:
-            with tools.chdir(self.source_subfolder):
-                self.copy("*", src="%s/builddir/install"%(os.getcwd()))
+        self.copy("*", src=os.path.join(self.build_folder, self._source_subfolder,"builddir","install"))
 
     def package_info(self):
-        #self.cpp_info.libs = ["taglib"]
         self.cpp_info.libs = tools.collect_libs(self)
 
